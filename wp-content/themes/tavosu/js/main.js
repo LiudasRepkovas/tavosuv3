0148f44566e6f58dcbd7e6be2f865824e0f2083a
Liudas Repkovas <repkovas@gmail.com>
2016-05-13 15:51:50 +0300

    form fixes

--- wp-content/themes/tavosu/js/main.js
+++ wp-content/themes/tavosu/js/main.js
@@ -5,4 +5,16 @@ $(document).ready(function(){
     // Target your .container, .wrapper, .post, etc.
     $(".content.container").fitVids();
     $(".instagram-media").css('max-width', '100%');
+
+    $('.ccf-submit-button').click(function(){
+        $('input').removeClass('red');
+        $('textarea').removeClass('red');
+        var form_errors = $('.error.required-error');
+        if(form_errors.length > 0){
+            $.each(form_errors, function(){
+                $(this).parent().find('input').addClass('red');
+                $(this).parent().find('textarea').addClass('red');
+            });
+        }
+    });
 });
